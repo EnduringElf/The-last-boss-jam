e53dfe12400af90dd8d6957dd505b7e331e9f16b
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2022-05-17 17:23:54 +0200

    update id ///c

--- Assets/Scripts/NpcDamageMech.cs
+++ Assets/Scripts/NpcDamageMech.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcDamageMech : MonoBehaviour
+public class NpcDamageMech : DamageMechanismMaster
 {
     // Start is called before the first frame update
     void Start()
