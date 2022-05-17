21186b334dbb1ed8fb9ec6a82d74d2ee7574971f
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2022-05-17 16:15:36 +0200

    scene update

--- Assets/Scripts/DamageMEchanism.cs
+++ Assets/Scripts/DamageMEchanism.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageMEchanism : MonoBehaviour
 {
+    [Header("Boss stats")]
+    public float HP = 100;
+    public float MaxHP = 100;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,37 @@ public class DamageMEchanism : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public bool DamageBoss(int value)
+    {
+        HP -= value;
+
+        if(HP <= 0)
+        {
+            BossDeath();
+        }
+        return true;
+    }
+
+    private void BossDeath()
+    {
+        Debug.Log("boss has died");
+    }
+
+    public bool HealBoos(int value)
+    {
+        if(HP != MaxHP && MaxHP - HP >= value)
+        {
+            HP += value;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
         
     }
 }
