0bf7345b14eaa5717276ee2f17a2b908bfd1e522
Mootet <69625548+Mootet@users.noreply.github.com>
2022-05-22 22:47:31 +0200

    WaveSpawner added

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,46 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[System.Serializable]
+ [System.Serializable]
 
-
-public class Wave
+ public class Wave
 {
     public string waveName;
     public int noOfEnemies;
-
+    public GameObject[] typeOfEnemies;
+    public float spawnInterval;
 }
+
 public class WaveSpawner : MonoBehaviour
 {
-    [SerializeField] Wave[] waves = new Wave[100];
-    [SerializeField]    public GameObject normalKnight;
-    [SerializeField]    public GameObject choomba;
-    [SerializeField]    public GameObject wizardo;
-    
-        void Start()
-        {
-            
-        Wave[] waves = new Wave[100];
-        setWave();
+     public Wave[] waves;
+    public Transform[] spawnPoints;
 
-        }
+    private Wave currentWave;
+    private int currentWaveNumber;
+    private float nextSpawnTime;
 
-    void Update()
+    private bool canSpawn = true;
+
+
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
 
-    public void setWave()
+    // Update is called once per frame
+    private void Update()
     {
-        for (int i = 1; i < 100; i++)
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+        GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (totalEnemies.Length == 0 && !canSpawn && currentWaveNumber+1 != waves.Length)
         {
-            waves[i-1].waveName = "Wave " + i;
-            waves[i-1].noOfEnemies = 3*i;
-            //waves[i-1].typeOfEnemies = 
+            currentWaveNumber++;
+            canSpawn = true;
         }
     }
 
+    void SpawnWave()
+    {
+        if (canSpawn && nextSpawnTime < Time.time)
+        {
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+            currentWave.noOfEnemies--;
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+            if(currentWave.noOfEnemies == 0)
+            {
+                canSpawn = false;
+            }
+        }
+        
 
+    }
 
 }
-
